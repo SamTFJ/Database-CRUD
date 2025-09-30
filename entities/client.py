7ec19d03a30a8f64aa85d26c01685f5ff393c1a0
VitorMSAraujo <vitor18manuelz@gmail.com>
2025-09-30 20:26:39 -0300

    Main.py Changes

--- entities/client.py
+++ entities/client.py
@@ -1,3 +1,7 @@
 from backend.supermarket import Supermarket
 from psycopg2 import sql
 supermarket = Supermarket()
+
+def close_connection():
+    supermarket.end_connection()
+    print("\n--> Connection closed.")
